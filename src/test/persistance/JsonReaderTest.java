56d7413fb5dd6e38f636eed6bd45c2e63578c481
miles <mepste01@students.cs.ubc.ca>
2020-10-28 15:20:44 -0700

    fixed syntax of test import
    Signed-off-by: miles <mepste01@students.cs.ubc.ca>

--- src/test/persistance/JsonReaderTest.java
+++ src/test/persistance/JsonReaderTest.java
@@ -1,7 +1,7 @@
 package persistance;
 
 import model.GameBoard;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 
